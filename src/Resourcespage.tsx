ac48ad345549a3d6592ba3126bf53bf6c97e9c5b
lphan48 <laurenphan229@gmail.com>
2023-10-15 17:39:36 -0400

    last minute changes

--- src/Resourcespage.tsx
+++ src/Resourcespage.tsx
@@ -2,9 +2,8 @@ import { Box, Grid, Typography } from "@mui/material";
 import budgeting from "./images/budgeting.jpeg";
 import credit from "./images/credit.jpeg";
 import investing from "./images/investing.jpeg";
-import TextField from '@mui/material/TextField'
-import React, {useState} from "react";
-;
+import TextField from "@mui/material/TextField";
+import React, { useState } from "react";
 function ResourcesPage() {
   const headingStyle = {
     fontFamily: "Bigshot",
@@ -22,7 +21,7 @@ function ResourcesPage() {
   const descriptionStyle = {
     fontFamily: "Times New Roman, sans-serif",
     fontSize: "23px",
-    paddingTop: 10,
+    paddingTop: 50,
     marginLeft: "20%",
     width: "60%",
   };
@@ -41,7 +40,7 @@ function ResourcesPage() {
 
   const [text, setTextt] = useState("");
   return (
-    <div className="About">
+    <div className="About" style={{ paddingBottom: 50 }}>
       <Typography style={headingStyle}>Her Money, Her Rules 💅</Typography>
       <Typography style={descriptionStyle}>
         An introduction to all things personal finance
@@ -51,12 +50,7 @@ function ResourcesPage() {
           paddingTop: 7,
         }}
       >
-        <Grid
-          container
-          spacing={10}
-          alignItems="center"
-          justifyContent="center"
-        >
+        <Grid container spacing={5} alignItems="center" justifyContent="center">
           <Grid item>
             <img
               src={budgeting}
@@ -73,39 +67,35 @@ function ResourcesPage() {
             <a
               href="https://www.ramseysolutions.com/budgeting/how-to-make-a-budget"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               How To Make A Budget <br />
             </a>
             <a
               href="https://www.investopedia.com/terms/b/budget.asp"
               target="_blank"
-              style = {linkStyle}
-
+              style={linkStyle}
             >
               10 Budgeting Mistakes Holding You Back <br />
             </a>
             <a
               href="https://www.discover.com/online-banking/banking-topics/5-budgeting-basics/"
               target="_blank"
-              style = {linkStyle}
-
+              style={linkStyle}
             >
               5 Essential Budgeting Basics <br />
             </a>
             <a
               href="https://www.futurelearn.com/info/courses/financial-analysis-business-performance-planning-budgeting-forecasting-sc/0/steps/192466"
               target="_blank"
-              style = {linkStyle}
-
+              style={linkStyle}
             >
               7 Approaches To Budgeting, Explained <br />
             </a>
             <a
               href="https://www.experian.com/blogs/ask-experian/budget-mistakes-to-avoid/"
               target="_blank"
-              style = {linkStyle}
-
+              style={linkStyle}
             >
               Common Budgeting Mistakes To Avoid <br />
             </a>
@@ -126,28 +116,28 @@ function ResourcesPage() {
             <a
               href="https://wallethub.com/edu/cs/how-to-build-credit/20579"
               target="_blank"
-              style = {linkStyle}
-              >
+              style={linkStyle}
+            >
               How To Build Credit In 8 Steps <br />
             </a>
             <a
               href="https://www.fool.com/the-ascent/credit-cards/how-credit-cards-work-beginners-guide/"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               How Do Credit Cards Work? <br />
             </a>
             <a
               href="https://www.nerdwallet.com/article/credit-cards/things-to-know-first-credit-card"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               11 Things To Know Before Opening Your First Credit Card <br />
             </a>
             <a
               href="https://www.nerdwallet.com/compare/credit-cards"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               Side By Side Credit Card Comparison <br />
             </a>
@@ -168,46 +158,44 @@ function ResourcesPage() {
             <a
               href="https://hbr.org/2021/08/how-to-make-smart-investments-a-beginners-guide"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               A Beginner's Guide To Investing <br />
             </a>
             <a
               href="https://www.bankrate.com/investing/best-investments-for-beginners/"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               Best Investments For Beginners <br />
             </a>
             <a
               href="https://money.usnews.com/investing/articles/best-investing-books-for-beginners"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               Introductory Investing Books <br />
             </a>
             <a
               href="https://www.investopedia.com/articles/stocks/07/beat_the_mistakes.asp"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               Avoid These Investing Mistakes <br />
             </a>
             <a
               href="https://smartasset.com/investing/types-of-investment"
               target="_blank"
-              style = {linkStyle}
+              style={linkStyle}
             >
               Different Investments And How They Work <br />
             </a>
           </Grid>
         </Grid>
       </Box>
-      <h4>  </h4>
-      <footer
-      style={descriptionStyle}
-      >
-          Any Questions? Ask Away! <br />
+      <h4> </h4>
+      <footer style={descriptionStyle}>
+        Any Questions? Ask Away! <br />
       </footer>
       <TextField
         hiddenLabel
@@ -215,24 +203,21 @@ function ResourcesPage() {
         variant="filled"
         size="small"
         sx={{
-          bgcolor: 'background.paper',
+          bgcolor: "background.paper",
           boxShadow: 1,
           borderRadius: 2,
           p: 2,
           minWidth: 600,
         }}
-        value = {text}
+        value={text}
         onChange={(e) => {
-          setTextt(e.target.value)
+          setTextt(e.target.value);
         }}
         onKeyDown={(event) => {
-          if (event.key === "Enter")
-            setTextt("")
-          }
-        }
+          if (event.key === "Enter") setTextt("");
+        }}
       />
     </div>
-  
   );
 }
 
